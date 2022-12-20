2c3d27919a463510c8f05c9cbcf4498c6c919161
Kenneth Usiobaifo <kenneth.usiobaifo@telvida.com>
2022-12-20 20:34:34 +0100

    tailwinds configured

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lox Corporate Services | Admin section</title>
-    <link href="dist/output.css" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
 </head>
 <body>
-    <section class="container">
-        <center>
-            <h1>Welcome back user</h1>
-            <a href="{{ url('/admin') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-            <a href="{{ url('/admin/cart') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Cart</a>
-            <a href="{{ url('/admin/orders') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Orders</a>
-            <a href="{{ url('/admin/products') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Products</a>
-        </center>
-        <h1 class="text-3xl font-bold underline">
-           Hello world!
-        </h1>
-    </section>
+  <h1 class="text-3xl font-bold underline">
+    Hello world! Admin Home
+  </h1>
 </body>
 </html>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lox Corporate Services | Admin section</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
 </head>
 <body>
-    <section class="container">
-        <center>
-            <h1>Products</h1>
-            <a href="{{ url('/admin') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-            <a href="{{ url('/admin/cart') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Cart</a>
-            <a href="{{ url('/admin/orders') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Orders</a>
-            <a href="{{ url('/admin/products') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Products</a>
-        </center>
-    </section>
+  <h1 class="text-3xl font-bold underline">
+    Hello world! Products
+  </h1>
 </body>
 </html>
